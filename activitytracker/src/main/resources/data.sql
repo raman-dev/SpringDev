627997f45c397ebc76826b30eeb576e1a587eee6
raman-dev <ramandeep.dev582@gmail.com>
2026-09-24 10:30:20 -0400

    Added default user into users table

--- activitytracker/src/main/resources/data.sql
+++ activitytracker/src/main/resources/data.sql
@@ -1,3 +1,5 @@
+insert into users (id, email, password) values (0,'raman@example.com','password');
+
 insert into activity (id, name,start_time_stamp,end_time_stamp) values ( 90, 'coding','8am','9am');
 insert into activity (id, name,start_time_stamp,end_time_stamp) values ( 91, 'coding','1230pm','130pm');
 insert into activity (id, name,start_time_stamp,end_time_stamp) values ( 92, 'watching youtube','1030am','12pm');
